590565cf87278cd324fbb48a3df5df002a71fc86
Prajesh Kotian <prajeshkotian@gmail.com>
2025-01-23 10:43:05 -0500

    Portfolio Changes

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -5,6 +5,8 @@ import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
 import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import '../assets/styles/Timeline.scss'
+import {data} from './../config'
+
 
 function Timeline() {
   return (
@@ -12,56 +14,22 @@ function Timeline() {
       <div className="items-container">
         <h1>Career History</h1>
         <VerticalTimeline>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
-            contentArrowStyle={{ borderRight: '7px solid  white' }}
-            date="2022 - present"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
-            icon={<FontAwesomeIcon icon={faBriefcase} />}
-          >
-            <h3 className="vertical-timeline-element-title">Technology Consultant</h3>
-            <h4 className="vertical-timeline-element-subtitle">Dallas, TX</h4>
-            <p>
-              Full-stack Web Development, GenAI/LLM, Project Management, Business Development
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2020 - 2022"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
-            icon={<FontAwesomeIcon icon={faBriefcase} />}
-          >
-            <h3 className="vertical-timeline-element-title">Full Stack Engineer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
-            <p>
-              Frontend Development, Backend Development, User Experience, Team Leading
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2021 - 2021"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
-            icon={<FontAwesomeIcon icon={faBriefcase} />}
-          >
-            <h3 className="vertical-timeline-element-title">Staff Engineer Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
-            <p>
-              Full-stack Development, API Development, User Experience
-            </p>
-          </VerticalTimelineElement>
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date="2020 - 2020"
-            iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
-            icon={<FontAwesomeIcon icon={faBriefcase} />}
-          >
-            <h3 className="vertical-timeline-element-title">Data Analyst Intern</h3>
-            <h4 className="vertical-timeline-element-subtitle">Tokyo, Japan</h4>
-            <p>
-              Automation, Data Governance, Statistical Analysis
-            </p>
-          </VerticalTimelineElement>
+        {(data?.careerHistory || []).map((item, index) => ( 
+            <VerticalTimelineElement
+              key={index} // Add a unique key for each element
+              className="vertical-timeline-element--work"
+              contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
+              contentArrowStyle={{ borderRight: '7px solid  white' }}
+              date={item.date}
+              iconStyle={{ background: '#5000ca', color: 'rgb(39, 40, 34)' }}
+              icon={<FontAwesomeIcon icon={faBriefcase} />}
+            >
+              <h3 className="vertical-timeline-element-title">{item.companyName}</h3>
+              <h3 className="vertical-timeline-element-title">{item.designation}</h3>
+              <h4 className="vertical-timeline-element-subtitle">{item.location}</h4>
+              <p>{item.description}</p>
+            </VerticalTimelineElement>
+          ))}
         </VerticalTimeline>
       </div>
     </div>
---
+++ src/config.js
@@ -0,0 +1,51 @@
+let data={
+    name:'Prajesh Kotian',
+    designation:'Software Engineer/Full-Stack Developer',
+    location: 'Fairfax, Virginia, USA',
+    education: 'MS CS Alumni George Mason University',
+    about: 'I am a software engineer with 4+ years of experience specializing in full-stack development using React, Node.js, TypeScript, and Python. I have a proven track record of building scalable, user-centric applications and optimizing system performance with tools like AWS and Docker. My background includes working on machine learning projects, such as developing BERT-based models, and collaborating with cross-functional teams in Agile environments.',
+    githubUrl:'https://github.com/prajeshkotian',
+    linkedinUrl: 'https://www.linkedin.com/in/prajesh-kotian/',
+    projects: [
+        {
+            title: 'EchoThread Web Application',
+            description: 'This project is an end-to-end encrypted chat application built using React.js, Node.js, MongoDB, and WebSockets. It ensures secure communication by implementing robust encryption standards to protect user data and maintain privacy. The application prioritizes data integrity and employs efficient real-time messaging through WebSockets, delivering a seamless and secure user experience.',
+            url: 'https://github.com/prajeshkotian/EchoThreadFE'
+        },
+        {
+            title: 'Factual Probing Research-Natural Language Processing',
+            description: 'This project implements Test-time Augmentation for Factual Probing, based on the EMNLP 2023 Findings paper. It explores enhancing factual probing by augmenting test prompts using methods like word swapping, back translation, and stopwords filtering. The augmented prompts are used with transformer models (e.g., T5) to generate candidate answers and aggregate results for improved accuracy.',
+            url: 'https://github.com/prajeshkotian/TTA4FactualProbing'
+        },
+        {
+            title: 'Tribe Group Chat Mobile App',
+            description: 'Tribe Group Chat Mobile App is a React Native and Expo project developed using TypeScript. The app facilitates group chats with features like real-time messaging, emoji support, and built-in reactions, creating a dynamic and engaging user experience',
+            url: 'https://github.com/prajeshkotian/Tribe-app'
+        },
+        {
+            title: 'Gentopia AI Agent',
+            description: 'This project involves building an AI agent using the Gentopia AI framework that answers questions based on George Mason University research papers. The agent utilizes OpenAI, Python, PyTorch, and Jupyter to process and extract relevant information from research papers, providing accurate and contextually relevant responses. It is powered by an LLM and leverages a manually updated dataset containing faculty research, enabling it to answer queries related to professors, their work, and academic topics at Mason.',
+            url: 'https://github.com/prajeshkotian/Gentopia-Mason-Prajesh'
+        },
+    ],
+    careerHistory: [
+        {
+            companyName: "DIAGNAL",
+            designation: "Software Engineer",
+            date: "Oct 2020-Jan 2023",
+            location: "Remote-India",
+            description: "FullStack Web Development, Guided a 4-member team in developing scalable features for a Video Content Management System (ENHANCE) using React js, Nodejs, GraphQL, HTML, CSS and DynamoDB (AWS), resulting in improved system performance and functionality"
+        },
+        {
+            companyName: "Torry Harris Integration Solutions",
+            designation: "Associate Software Engineer",
+            date: "Sept 2018-Oct 2020",
+            location: "Bengaluru, India",
+            description: "Software Engineer/Devops Engineer, Developed scalable REST APIs using Python and MongoDB, supporting over 400,000 customers and driving highperformance feature deployment, Reduced deployment time by 45% using Helm Charts and improved scalability through Kubernetes Orchestration."
+        }
+
+    ],
+
+}
+
+export {data};
