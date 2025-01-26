716fbdbce4c9b800c47dbfb3d77e45dff109dfc9
Prajesh Kotian <prajeshkotian@gmail.com>
2025-01-26 12:27:37 -0500

    removed unused variables

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,8 +1,6 @@
-import React from "react";
 import mock01 from '../assets/images/mock01.png';
 import mock02 from '../assets/images/mock02.png';
 import mock03 from '../assets/images/mock03.png';
-import mock10 from '../assets/images/mock10.png';
 import mock11 from './../assets/images/mock11.png'
 import '../assets/styles/Project.scss';
 
